dd7448e130e597cac627dd19c12573384a18728f
MNishita <mnishita1029@gmail.com>
2021-07-04 14:34:42 +0530

    completed cart actions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import Navbar from "./Components/Navbar";
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Cart from './Components/Cart'
 
-function App() {
-  return (
-    <Navbar />
-  );
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
--- src/Components/reducers/cartReducer.js
+++ src/Components/reducers/cartReducer.js
@@ -4,6 +4,7 @@ import Item3 from '../../images/item3.jpg'
 import Item4 from '../../images/item4.jpg'
 import Item5 from '../../images/item5.jpg'
 import Item6 from '../../images/item6.jpg'
+import {ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY} from '../actions/actionTypes/cart-actions'
 
 
 const initState = {
@@ -20,8 +21,71 @@ const initState = {
 
 }
 const cartReducer= (state = initState,action)=>{
-    
-    return state;
 
+    if(action.type === ADD_TO_CART) {
+        let addedItem = state.items.find(item=> item.id === action.id)
+        let existed_item= state.addedItems.find(item=> action.id === item.id)
+        if(existed_item) {
+            addedItem.quantity += 1;
+            return {
+                ...state,
+                total: state.total + addedItem.price
+            }
+        }
+        else {
+            addedItem.quantity = 1;
+            let newTotal = state.total + addedItem.price;
+            return {
+                ...state,
+                addedItems: [...state.addedItems, addedItem],total : newTotal
+            }
+        }
+    }
+
+    if(action.type === REMOVE_ITEM) {
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return {
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id)
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+    return state
 }
 export default cartReducer;
