59b1664023ee2b836d1d4a8f453fda92bdde909c
MNishita <mnishita1029@gmail.com>
2021-07-04 18:07:29 +0530

    added new items

--- src/Components/reducers/cartReducer.js
+++ src/Components/reducers/cartReducer.js
@@ -2,19 +2,31 @@ import Item1 from '../../images/item1.jpg'
 import Item2 from '../../images/item2.jpg'
 import Item3 from '../../images/item3.jpg'
 import Item4 from '../../images/item4.jpg'
-import Item5 from '../../images/item5.jpg'
+import Item5 from '../../images/item5.jpeg'
 import Item6 from '../../images/item6.jpg'
+import Item7 from '../../images/item7.jpg'
+import Item8 from '../../images/item8.jpg'
+import Item9 from '../../images/item9.jpg'
+import Item10 from '../../images/item10.jpg'
+import Item11 from '../../images/item11.jpg'
+import Item12 from '../../images/item12.jpg'
 import {ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY} from '../actions/actionTypes/cart-actions'
 
 
 const initState = {
     items: [
-        {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item1},
-        {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item2},
-        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item3},
-        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item4},
-        {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item5},
-        {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item6}
+        {id:1,title:'Bracelet', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item1},
+        {id:2,title:'Black Cat', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item2},
+        {id:3,title:'Quartz', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item3},
+        {id:4,title:'', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item4},
+        {id:5,title:'', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item5},
+        {id:6,title:'', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item6},
+        {id:7,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item7},
+        {id:8,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item8},
+        {id:9,title:'Winter Body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item9},
+        {id:10,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item10},
+        {id:1,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item11},
+        {id:6,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item12}
     ],
     addedItems:[],
     total: 0
